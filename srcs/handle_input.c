0b7f90f8d19c346061768eeb4d952ccabd73061b
Yuleum Park <yuleumpark@Yuleums-MacBook-Pro.local>
2025-05-08 22:36:43 +0200

    upload on github

--- srcs/handle_input.c
+++ srcs/handle_input.c
@@ -6,7 +6,7 @@
 /*   By: yulpark <yulpark@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 00:25:24 by yulpark           #+#    #+#             */
-/*   Updated: 2025/05/02 00:50:34 by yulpark          ###   ########.fr       */
+/*   Updated: 2025/05/05 15:21:35 by yulpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,21 +35,43 @@ int	ft_atoi(const char *nptr)
 	return (sign * result);
 }
 
+int input_checker(char **args)
+{
+	int i;
+	int	j;
+
+	i = 0;
+	while (args[i])
+	{
+		j = 0;
+		while (args[i][j])
+		{
+			if (!(args[i][j] >= '0' && args[i][j] <= '9'))
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
 int init_input(int argc, char *argv[])
 {
-	t_arg	*args;
+	t_arg	*main_data;
 	
 	if (argc < 5 || argc > 7)
 		return (print_error(ERR_INVALID_ARG), 1);
 	if (ft_atoi(argv[1]) < 1)
 		return (print_error(ERR_INVALID_ARG), 1);
-	args->num_philo = ft_atoi(argv[1]);
-	args->t_die = ft_atoi(argv[2]);
-	args->t_eat = ft_atoi(argv[3]);
-	args->t_sleep = ft_atoi(argv[4]);
+	if (input_checker(argv) != 0)
+		return (print_error(ERR_INVALID_ARG), 1);
+	main_data->num_philo = ft_atoi(argv[1]);
+	main_data->t_die = ft_atoi(argv[2]);
+	main_data->t_eat = ft_atoi(argv[3]);
+	main_data->t_sleep = ft_atoi(argv[4]);
 	if (argv[5])
-		args->t_must_eat = ft_atoi(argv[5]);
+		main_data->t_must_eat = ft_atoi(argv[5]);
 	else
-		args->t_must_eat = 0;
+		main_data->t_must_eat = 0;
 	return (SUCCESS);
 }
